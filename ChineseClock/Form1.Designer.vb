d3c087791ab6b7f63e4a0cc6763d3e7a7b3d171d
levbondarev <x-corpo@live.ru>
2020-11-04 15:09:29 +0300

    Adding files
    Adding the files of the solution

---
+++ ChineseClock/Form1.Designer.vb
@@ -0,0 +1,58 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Форма переопределяет dispose для очистки списка компонентов.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Является обязательной для конструктора форм Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'Примечание: следующая процедура является обязательной для конструктора форм Windows Forms
+    'Для ее изменения используйте конструктор форм Windows Form.  
+    'Не изменяйте ее в редакторе исходного кода.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.NotifyIcon1 = New System.Windows.Forms.NotifyIcon(Me.components)
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'NotifyIcon1
+        '
+        Me.NotifyIcon1.Text = "MyIcon"
+        Me.NotifyIcon1.Visible = True
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 500
+        '
+        'Timer2
+        '
+        Me.Timer2.Interval = 10000
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents NotifyIcon1 As System.Windows.Forms.NotifyIcon
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents Timer2 As System.Windows.Forms.Timer
+
+End Class
---
+++ ChineseClock/Form1.vb
@@ -0,0 +1,71 @@
+﻿Public Class Form1
+    Public formtext As String
+    Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        Timer1.Start()
+        Timer2.Start()
+        Timer2_Tick(New Object, System.EventArgs.Empty)
+        Timer1_Tick(New Object, System.EventArgs.Empty)
+    End Sub
+
+    Private Sub Timer2_Tick(sender As System.Object, e As System.EventArgs) Handles Timer2.Tick
+        If formtext = "乙" Then
+            Icon = My.Resources.Йи
+        ElseIf formtext = "甲" Then
+            Icon = My.Resources.Цзя
+        ElseIf formtext = "丙" Then
+            Icon = My.Resources.Бинь
+        ElseIf formtext = "丁" Then
+            Icon = My.Resources.Динь
+        ElseIf formtext = "戊" Then
+            Icon = My.Resources.У
+        ElseIf formtext = "己" Then
+            Icon = My.Resources.Цзи
+        ElseIf formtext = "庚" Then
+            Icon = My.Resources.Гэн
+        ElseIf formtext = "辛" Then
+            Icon = My.Resources.Син
+        ElseIf formtext = "壬" Then
+            Icon = My.Resources.Жэн
+        ElseIf formtext = "癸" Then
+            Icon = My.Resources.Гуй
+        End If
+    End Sub
+
+    Private Sub Timer1_Tick(sender As System.Object, e As System.EventArgs) Handles Timer1.Tick
+        Dim milliseconds As Double = (Now.Hour * 60 * 60) + (Now.Minute * 60) + (Now.Second) '+ (Now.Millisecond / 100)
+        Dim miao As Double = milliseconds / 86400 * 100000
+        Dim wenmiao = Math.Round(miao).ToString
+rt:
+        If wenmiao.Length < 5 Then
+            wenmiao = "0" + wenmiao
+            GoTo rt
+        End If
+        If wenmiao(0) = "0" Then
+            formtext = "丙"
+        ElseIf wenmiao(0) = "1" Then
+            formtext = "丁"
+        ElseIf wenmiao(0) = "2" Then
+            formtext = "戊"
+        ElseIf wenmiao(0) = "3" Then
+            formtext = "己"
+        ElseIf wenmiao(0) = "4" Then
+            formtext = "庚" '+ wenmiao.Substring(1)
+        ElseIf wenmiao(0) = "5" Then
+            formtext = "辛" '+ wenmiao.Substring(1)
+        ElseIf wenmiao(0) = "6" Then
+            formtext = "壬" '+ wenmiao.Substring(1)
+        ElseIf wenmiao(0) = "7" Then
+            formtext = "癸" '+ wenmiao.Substring(1)
+        ElseIf wenmiao(0) = "8" Then
+            formtext = "甲" '+ wenmiao.Substring(1)
+        ElseIf wenmiao(0) = "9" Then
+            formtext = "乙" '+ wenmiao.Substring(1)
+        End If
+        Text = wenmiao.Substring(1, 2) + ":" + wenmiao.Substring(3, 2) ' + "." + wenmiao.Substring(4, 2)
+    End Sub
+
+    Private Sub Form1_Activated(sender As System.Object, e As System.EventArgs) Handles MyBase.Activated
+        Location = New Point(My.Computer.Screen.WorkingArea.Height, My.Computer.Screen.WorkingArea.Width)
+
+    End Sub
+End Class
